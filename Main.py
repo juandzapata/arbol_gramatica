acd82532e6a95c0bda9c97fab975c5ae0645d53b
juandzapata <juan.zapata22871@ucaldas.edu.co>
2024-04-02 20:37:03 -0500

    Incrementación lógica de la generación del árbol + Modificación en la impresión del árbol

--- Main.py
+++ Main.py
@@ -3,9 +3,10 @@ from Nodizador import Nodizador
 
 def main():
     listaSeguimiento = ['S', 'S', 'a', 'S', 'A', 'A', 'a', 'A', 'A', 'A', 'b', 'A', 'B', 'B', 'λ']
-    listaSeguimiento = ['S', 'S', 'L', 'S', 'R', 'L', 'ab', 'R', 'cd']
+    listaSeguimiento2 = ['S', 'S', 'L', 'S', 'R', 'L', 'ab', 'R', 'cd']
     listaSeguimiento3 = ['S', 'S', 'a', 'S', 'B', 'B', 'b', 'B', "B'", "B'", 'S', 'S', 'a', 'S', 'B', 'B', 'b', 'B', "B'", "B'", 'λ']
-    nodizador = Nodizador(listaSeguimiento3)
+    listaSeguimiento4 = ['S', 'S', 'a', 'S', 'B', 'B', 'b', 'B', "B'", "B'" , 'λ']
+    nodizador = Nodizador(listaSeguimiento4)
     nodizador.mostrarNodos()
     nodizador.mostrarArbol()
 
